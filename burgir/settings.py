cc9e98848da18c1b8b692b653a88f0065ff09783
epelix <90451715+eemeliHuotari@users.noreply.github.com>
2025-05-17 20:38:14 +0300

    Update settings.py

--- burgir/settings.py
+++ burgir/settings.py
@@ -31,7 +31,6 @@ USE_X_FORWARDED_PORT = True
 
 # Required for OpenShift health checks
 ALLOWED_HOSTS = [
-    'test-api-distributed-ransomware-detection-file-recovery.2.rahtiapp.fi',
     'helionmunvaimo.2.rahtiapp.fi',
     'localhost',
     '127.0.0.1',
